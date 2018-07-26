6265377b1e00537ef889bfa2bc024f31f8b0a77b
natansevero <natanseverolp@gmail.com>
2018-07-25 22:21:58 -0300

    criando e adicionando novos componentes a dashboard

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-    <div>
-        <h1>Dashboard</h1>
-    </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import ValueBox from '../common/widget/valueBox' 
+import Row from '../common/layout/row'
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Dashboard' small='Versão 1.0' />
+                <Content>
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank' 
+                            value='R$ 10' text='Total de Créditos' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card' 
+                            value='R$ 10' text='Total de Débitos' />
+                        <ValueBox cols='12 4' color='blue' icon='money' 
+                            value='R$ 0' text='Total de Consolidade' />
+                    </Row>
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
