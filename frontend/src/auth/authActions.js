3f8f34e6a9008c2afee931b98604dde50d70485b
natansevero <natanseverolp@gmail.com>
2018-08-02 22:09:07 -0300

    definindo actions e reducer para auth

---
+++ frontend/src/auth/authActions.js
@@ -0,0 +1,43 @@
+import { toastr } from 'react-redux-toastr'
+import axios from 'axios'
+import consts from '../consts'
+
+export function login(values) {
+    return submit(values, `${consts.OAPI_URL}/login`)
+}
+
+export function singup(values) {
+    return submit(values, `${consts.OAPI_URL}/singup`)
+}
+
+function submit(values, url) {
+    return dispatch => {
+        axios.post(url, values)
+            .then(res => {
+                dispatch([
+                    { type: 'USER_FETCHED', payload: res.data }
+                ])
+            })
+            .catch(e => {
+                e.response.data.errors.forEach(error => toastr.error('Erro', error))
+            })
+    }
+}
+
+export function logout() {
+    return { type: 'TOKEN_VALIDATED', payload: false }
+}
+
+export function validateToken(token) {
+    return dispatch => {
+        if(token) {
+            axios.post(`${consts.OAPI_URL}/validateToken`, { token })
+                .then(res => {
+                    dispatch({ type: 'TOKEN_VALIDATED', payload: res.data.valid })
+                })
+                .catch(e => dispatch({ type: 'TOKEN_VALIDATED', payload: false }))
+        } else {
+            dispatch({ type: 'TOKEN_VALIDATED', payload: false })
+        }
+    }
+}
---
+++ frontend/src/consts.js
@@ -0,0 +1,5 @@
+export default {
+    API_URL: 'http://localhost:3003/api',
+    // OPEN API
+    OAPI_URL: 'http://localhost:3003/oapi'
+}
