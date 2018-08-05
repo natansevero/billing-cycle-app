5397b79272a7fcca3284a4e31c6ab2e4f027c075
natansevero <natanseverolp@gmail.com>
2018-08-04 22:18:21 -0300

    configurando o deploy do frontend

--- frontend/src/consts.js
+++ frontend/src/consts.js
@@ -1,5 +1,8 @@
 export default {
-    API_URL: 'http://localhost:3003/api',
+    // API_URL: 'http://localhost:3003/api',
+    // // OPEN API
+    // OAPI_URL: 'http://localhost:3003/oapi'
+    API_URL: 'https://natan-my-money-app-backend.herokuapp.com/api',
     // OPEN API
-    OAPI_URL: 'http://localhost:3003/oapi'
+    OAPI_URL: 'https://natan-my-money-app-backend.herokuapp.com/oapi'
 }
