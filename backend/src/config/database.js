48fb422ed411bb96a961c7c4142c1a0bd2e533a3
natansevero <natanseverolp@gmail.com>
2018-08-04 21:43:12 -0300

    config deploy to heroku

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/mymoney'
+module.exports = mongoose.connect(url)
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = 
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,4 +1,4 @@
-const port = 3003
+const port = process.env.PORT || 3003
 
 const bodyParser = require('body-parser')
 const express = require('express')
